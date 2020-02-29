4717e6527dcfcc0d52233e3762ab4e233ef3b90d
LayersOfAbstraction <jnash51@live.com>
2020-02-29 12:56:21 +1000

    Update types and validation

--- application/Pitcher/Models/User.cs
+++ application/Pitcher/Models/User.cs
@@ -7,74 +7,82 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Pitcher.Models
 {
     public class User
-    {
+    {      
         public int ID { get; set; }
 
         [Required]
-        [StringLength(30, MinimumLength = 2, ErrorMessage = "* Last Name be bettween 2 to 30 characters.")]
-        [Display(Name = "Last Name")]
-        public string UserLastName { get; set; }
-
-        [Required]
         [StringLength(20, MinimumLength = 2, ErrorMessage = "* First Name be bettween 2 to 20 characters.")]
+        [DataType(DataType.Text)]
         [Display(Name = "First Name")]
         [Column("UserFirstName")]
-        public string UserFirstName { get; set; }     
+        public string UserFirstName { get; set; }   
+
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "* Last Name be bettween 2 to 30 characters.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        [Column("UserLastName")]
+        public string UserLastName { get; set; }  
         
-        [Display(Name = "Is A Leader?")]
+        [Display(Name = "Is A Leader?")]        
         [Column("UserIsLeader")]
         public bool UserIsLeader{get;set;}
                 
         [Required]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Email address be bettween 3 to 30 characters.")]
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         [Column("UserContactEmail")]
-        public string UserContactEmail{get;set;}
+        public string UserContactEmail{get;set;}        
         
-        [StringLength(20, MinimumLength = 5,ErrorMessage = "Phone Number must be bettween 5 to 30 characters." +
-            "Advise you input the full international number if user is out of state.")]
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         [Column("UserPhoneNumber")]
         public long UserPhoneNumber{get;set;}
         
         [StringLength(37,ErrorMessage = "Address cannot be longer than 37 characters.")]
+        [DataType(DataType.Text)]
         [Display(Name = "Address")]
         [Column("UserAddress")]
         public string UserAddress{get;set;}
-
-        [StringLength(32, MinimumLength = 2, ErrorMessage = "Post Code must be bettween 2 to 32 characters.")]
+        
+        //This regular expression allows valid postcodes and not just USA Zip codes.        
         [Display(Name = "Post Code")]
-        [Column("UserPostCode")]
-        public int UserPostCode {get;set;}
+        [Column("UserPostCode")][DataType(DataType.PostalCode)]
+        public string UserPostCode { get; set; }
 
         [StringLength(15,ErrorMessage = "Country cannot be longer than 15 characters.")]
+        [DataType(DataType.Text)]
         [Display(Name = "Country")]
         [Column("UserCountry")] 
         public string UserCountry {get;set;}
-
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Mobile Number must be bettween 5 to 20 characters." +
-            "Advise you input the full international number if user is out of state.")]
+        
+        //This regular expression allows valid mobile numbers throughout the world.
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Mobile Number")]
         [Column("UserMobileNumber")]
         public long UserMobileNumber {get;set;}
 
         [StringLength(3,ErrorMessage = "State cannot be longer than 3 characters.")]
+        [DataType(DataType.Text)]
         [Display(Name = "State")]
         [Column("UserState")]
         public string UserState {get;set;}
 
         [Required]
-        [StringLength(16, MinimumLength = 1,ErrorMessage = "* Username must be bettween 1 to 16 characters.")]
+        [DataType(DataType.Text)]
+        [StringLength(16, MinimumLength = 3,ErrorMessage = "Username must be bettween 1 to 16 characters.")]
         [Display(Name = "Username")]
         [Column("UserLogInName")]
         public string UserLogInName {get;set;}
 
         [Required]
-        [StringLength(30, MinimumLength = 8, ErrorMessage = "* Password Cannot be longer than 18 characters.")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be bettween 8 to 30 characters.")]
+        [DataType(DataType.Password)]
         [Display(Name = "Password")]
         [Column("UserPassword")]
         public string UserPassword {get;set;}
 
+        public ICollection<Registration> Registrations {get;set;}
     }
 }
