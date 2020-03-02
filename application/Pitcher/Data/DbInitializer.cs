f61f8a28ec92fe751af41d0182d2210d58056e61
LayersOfAbstraction <jnash51@live.com>
2020-03-02 15:30:16 +1000

    Recreate file

--- application/Pitcher/Data/DbInitializer.cs
+++ application/Pitcher/Data/DbInitializer.cs
@@ -18,9 +18,9 @@ namespace Pitcher.Data
 
             var users = new User[]
             {
-                new User{UserFirstName="Carson",UserLastName="Alexander",UserIsLeader=false,UserContactEmail="calex51@gmail.com",UserPhoneNumber=61071800671518,UserAddress="54 Yankee st Runcorn",UserPostCode="4113",UserCountry="Australia",UserMobileNumber=610476162089,UserState="QLD",UserLogInName="BioGlands",UserPassword=")Bstd..%E=)_(:7"},
-                new User{UserFirstName="Alonso",UserLastName="Meredith",UserIsLeader=true,UserContactEmail="am15@hotmail.com",UserPhoneNumber=61038002222,UserAddress="34 Webster Dandenong Park",UserPostCode="3175",UserCountry="Australia",UserMobileNumber=610423162085,UserState="VIC",UserLogInName="Bioshock32",UserPassword=")Bc--..3E=*_!j2"},
-                new User{UserFirstName="Arturo",UserLastName="Anand",UserIsLeader=true,UserContactEmail="aa01@live.com",UserPhoneNumber=61028004278,UserAddress="72 Doug Rd Wakevale",UserPostCode="3115",UserCountry="Australia",UserMobileNumber=610423162085,UserState="VIC",UserLogInName="IdaSurvior",UserPassword="8w7n9$xY{#'i6w:ud-<.9fFDCU<-Gh"},                
+                new User{ UserFirstName="Carson",UserLastName="Alexander",UserIsLeader=false,UserContactEmail="calex51@gmail.com",UserPhoneNumber=61071800671518,UserAddress="54 Yankee st Runcorn",UserPostCode="4113",UserCountry="Australia",UserMobileNumber=610476162089,UserState="QLD",UserLogInName="BioGlands",UserPassword=")Bstd..%E=)_(:7"},
+                new User{ UserFirstName="Alonso",UserLastName="Meredith",UserIsLeader=true,UserContactEmail="am15@hotmail.com",UserPhoneNumber=61038002222,UserAddress="34 Webster Dandenong Park",UserPostCode="3175",UserCountry="Australia",UserMobileNumber=610423162085,UserState="VIC",UserLogInName="Bioshock32",UserPassword=")Bc--..3E=*_!j2"},
+                new User{ UserFirstName="Arturo",UserLastName="Anand",UserIsLeader=true,UserContactEmail="aa01@live.com",UserPhoneNumber=61028004278,UserAddress="72 Doug Rd Wakevale",UserPostCode="3115",UserCountry="Australia",UserMobileNumber=610423162085,UserState="VIC",UserLogInName="IdaSurvior",UserPassword="8w7n9$xY{#'i6w:ud-<.9fFDCU<-Gh"},                
             };
 
             foreach (User u in users)
@@ -31,9 +31,9 @@ namespace Pitcher.Data
 
             var registrations = new Registration[]
             {
-                new Registration{ID=1, UserID=1, JobID=1, RegistrationDate=DateTime.Parse("2005-09-01")},
-                new Registration{ID=2, UserID=2, JobID=2, RegistrationDate=DateTime.Parse("2004-03-07")},
-                new Registration{ID=3, UserID=3, JobID=3, RegistrationDate=DateTime.Parse("2004-02-13")}
+                new Registration{UserID=1, JobID=1, RegistrationDate=DateTime.Parse("2005-09-01")},
+                new Registration{UserID=2, JobID=2, RegistrationDate=DateTime.Parse("2004-03-07")},
+                new Registration{UserID=3, JobID=3, RegistrationDate=DateTime.Parse("2004-02-13")}
             };
 
             foreach (Registration r in registrations)
@@ -45,9 +45,9 @@ namespace Pitcher.Data
             //This means projects
             var jobs = new Job[]
             {
-                new Job{ID=1, JobTitle="Eskate Tracker",JobDescription="A fitness tracker designed for inline skaters.",JobStartDate=DateTime.Parse("2004-03-07"),JobDeadline=DateTime.Parse("2003-12-07"),JobIsComplete=false},
-                new Job{ID=2, JobTitle="FTDC",JobDescription="A windows desktop app that renames files with time and date.",JobStartDate=DateTime.Parse("2004-03-07"),JobDeadline=DateTime.Parse("2003-08-07"),JobIsComplete=false},
-                new Job{ID=3, JobTitle="Pitcher",JobDescription="This app.",JobStartDate=DateTime.Parse("2020/02/10"),JobDeadline=DateTime.Parse("2020-07-2"), JobIsComplete=false}                                                              
+                new Job{JobTitle="Eskate Tracker",JobDescription="A fitness tracker designed for inline skaters.",JobStartDate=DateTime.Parse("2004-03-07"),JobDeadline=DateTime.Parse("2003-12-07"),JobIsComplete=false},
+                new Job{JobTitle="FTDC",JobDescription="A windows desktop app that renames files with time and date.",JobStartDate=DateTime.Parse("2004-03-07"),JobDeadline=DateTime.Parse("2003-08-07"),JobIsComplete=false},
+                new Job{JobTitle="Pitcher",JobDescription="This app.",JobStartDate=DateTime.Parse("2020/02/10"),JobDeadline=DateTime.Parse("2020-07-2"), JobIsComplete=false}                                                              
             };
 
             foreach (Job j in jobs)
