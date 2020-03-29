027d4a807168c10f3059197f79f433328ac8df15
LayersOfAbstraction <jnash51@live.com>
2020-03-29 10:31:20 +1000

    Add Login and Logout methods

--- application/Pitcher/Controllers/UsersController.cs
+++ application/Pitcher/Controllers/UsersController.cs
@@ -8,6 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using Pitcher.Data;
 using Pitcher.Models;
 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+
 namespace Pitcher.Controllers
 {
     public class UsersController : Controller
@@ -17,6 +22,24 @@ namespace Pitcher.Controllers
         public UsersController(TeamContext context)
         {
             _context = context;
+        }
+
+        public async Task Login(string returnUrl = "/")
+        {
+            await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() {RedirectUri = returnUrl});
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
+            {
+                // Indicate here where Auth0 should redirect the user after a logout.
+                // Note that the resulting absolute Uri must be whitelisted in the
+                // **Allowed Logout URLs** settings for the app.
+                RedirectUri = Url.Action("Index", "Home")
+            });
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
 
         // GET: Users
