99030565e4ea77b576993530b60e35ed416760b2
LayersOfAbstraction <jnash51@live.com>
2020-05-04 21:21:04 +1000

    Rename ProblemID to ID

---
+++ application/Pitcher/Controllers/ProblemsController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Pitcher.Data;
+using Pitcher.Models;
+
+namespace Pitcher.Controllers
+{
+    public class ProblemsController : Controller
+    {
+        private readonly TeamContext _context;
+
+        public ProblemsController(TeamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Problems
+        public async Task<IActionResult> Index()
+        {
+            var teamContext = _context.Problem.Include(p => p.Job);
+            return View(await teamContext.ToListAsync());
+        }
+
+        // GET: Problems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var problem = await _context.Problem
+                .Include(p => p.Job)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (problem == null)
+            {
+                return NotFound();
+            }
+
+            return View(problem);
+        }
+
+        // GET: Problems/Create
+        public IActionResult Create()
+        {
+            ViewData["JobID"] = new SelectList(_context.Jobs, "ID", "JobTitle");
+            return View();
+        }
+
+        // POST: Problems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,JobID,RegistrationID,ProblemTitle,ProblemDescription,ProblemStartDate,ProblemFileAttachments,ProblemSeverity,ProblemComplete")] Problem problem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(problem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobID"] = new SelectList(_context.Jobs, "ID", "JobTitle", problem.JobID);
+            return View(problem);
+        }
+
+        // GET: Problems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var problem = await _context.Problem.FindAsync(id);
+            if (problem == null)
+            {
+                return NotFound();
+            }
+            ViewData["JobID"] = new SelectList(_context.Jobs, "ID", "JobTitle", problem.JobID);
+            return View(problem);
+        }
+
+        // POST: Problems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,JobID,RegistrationID,ProblemTitle,ProblemDescription,ProblemStartDate,ProblemFileAttachments,ProblemSeverity,ProblemComplete")] Problem problem)
+        {
+            if (id != problem.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(problem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProblemExists(problem.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobID"] = new SelectList(_context.Jobs, "ID", "JobTitle", problem.JobID);
+            return View(problem);
+        }
+
+        // GET: Problems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var problem = await _context.Problem
+                .Include(p => p.Job)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (problem == null)
+            {
+                return NotFound();
+            }
+
+            return View(problem);
+        }
+
+        // POST: Problems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var problem = await _context.Problem.FindAsync(id);
+            _context.Problem.Remove(problem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProblemExists(int id)
+        {
+            return _context.Problem.Any(e => e.ID == id);
+        }
+    }
+}
