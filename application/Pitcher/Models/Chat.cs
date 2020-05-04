34be5e56491dfee77c319e6b2acf04f245bb0e53
LayersOfAbstraction <jnash51@live.com>
2020-05-04 21:42:31 +1000

    Add Chat

--- application/Pitcher/Models/Chat.cs
+++ application/Pitcher/Models/Chat.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace Pitcher.Models
 {
     public class Chat
@@ -7,8 +12,18 @@ namespace Pitcher.Models
 
         public int RegistrationID {get;set;}
 
+        [Required]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "Chat Description must be bettween 255 to 3 characters.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Description")]
+        [Column("ChatDescription")]
         public string ChatDescription {get;set;}
 
+        [Required]
+        [DataType(DataType.Date)]//AUTO INSERT.
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = " Problem Start Date")]
+        [Column("ProblemStartDate")]
         public string ChatPublishDate {get;set;}
 
         public Registration Registration {get;set;}
