701b1b00b3327beeb90d855f53349b5ddc927b47
LayersOfAbstraction <jnash51@live.com>
2020-05-04 21:09:47 +1000

    Add FKs

---
+++ application/Pitcher/Models/Problem.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pitcher.Models
+{
+    public class Problem
+    {
+        public int ID {get;set;}
+
+        public int JobID {get;set;}
+
+        public int RegistrationID {get;set;}
+        
+        [Required]
+        [StringLength(180, MinimumLength = 2, ErrorMessage = "Problem Title must be bettween 2 to 20 characters.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Problem Title")]
+        [Column("ProblemTitle")]
+        public string ProblemTitle {get;set;}
+
+        [Required]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Problem Title must be bettween 2 to 255 characters.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Problem Description")]
+        [Column("ProblemDescription")]
+        public string ProblemDescription {get;set;}
+
+        [Required]
+        [DataType(DataType.Date)]//AUTO INSERT.
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = " Problem Start Date")]
+        [Column("ProblemStartDate")]
+        public DateTime ProblemStartDate {get;set;}
+
+        [DataType(DataType.Upload)]
+        [Display(Name = " Upload file")]
+        [Column("ProblemFileAttachments")]
+        public string ProblemFileAttachments {get;set;}
+
+        [Required]
+        [Display(Name = "Problem Severity")]        
+        [Column("ProblemSeverity")]
+        public int ProblemSeverity {get;set;}
+
+        [Display(Name = "Problem Complete")]        
+        [Column("ProblemComplete")]        
+        public bool ProblemComplete {get;set;}
+        
+        public Registration Registration {get;set;}
+        public Job Job {get;set;}
+
+        public ICollection<Chat> Chat {get;set;}
+    }
+}
