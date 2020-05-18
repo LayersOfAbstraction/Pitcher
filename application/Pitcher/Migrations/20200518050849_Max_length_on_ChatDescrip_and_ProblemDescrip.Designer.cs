5928cfc9e349201405ed3949f6b34239eae9f955
LayersOfAbstraction <jnash51@live.com>
2020-05-18 18:29:02 +1000

    Add Max_length for tblProblems and tblChat and then model TeamContextModelSnapshot

--- application/Pitcher/Migrations/20200518050849_Max_length_on_ChatDescrip_and_ProblemDescrip.Designer.cs
+++ application/Pitcher/Migrations/20200518050849_Max_length_on_ChatDescrip_and_ProblemDescrip.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pitcher.Data;
 
 namespace Pitcher.Migrations
 {
     [DbContext(typeof(TeamContext))]
-    partial class TeamContextModelSnapshot : ModelSnapshot
+    [Migration("20200518050849_Max_length_on_ChatDescrip_and_ProblemDescrip")]
+    partial class Max_length_on_ChatDescrip_and_ProblemDescrip
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
