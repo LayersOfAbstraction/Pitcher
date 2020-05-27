2ab51c5239b233a1f0613d2dd26273fbba71f6ce
LayersOfAbstraction <jnash51@live.com>
2020-05-27 17:14:27 +1000

    Take out FK attributes

--- application/Pitcher/Models/Result.cs
+++ application/Pitcher/Models/Result.cs
@@ -6,10 +6,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Pitcher.Models
 {
     public class Result
-    {
+    {        
         public int JobID {get;set;}
+        
         public int ProblemID {get;set;}
-
         public Job Job {get;set;}
         public Problem Problem {get;set;}
 
