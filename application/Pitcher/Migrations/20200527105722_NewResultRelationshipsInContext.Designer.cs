283961c358c442a98272aef17542bcb8e5f37711
LayersOfAbstraction <jnash51@live.com>
2020-05-28 21:08:26 +1000

    Add snapshots and new relationship updates

--- application/Pitcher/Migrations/20200527105722_NewResultRelationshipsInContext.Designer.cs
+++ application/Pitcher/Migrations/20200527105722_NewResultRelationshipsInContext.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pitcher.Data;
 
 namespace Pitcher.Migrations
 {
     [DbContext(typeof(TeamContext))]
-    partial class TeamContextModelSnapshot : ModelSnapshot
+    [Migration("20200527105722_NewResultRelationshipsInContext")]
+    partial class NewResultRelationshipsInContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
