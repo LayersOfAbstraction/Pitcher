b4ac72cbd0c5d81a6c0a7c4242c0a2ba56aaf7ff
LayersOfAbstraction <jnash51@live.com>
2020-06-08 18:37:14 +1000

    Add PK

--- application/Pitcher/Models/Result.cs
+++ application/Pitcher/Models/Result.cs
@@ -7,11 +7,11 @@ namespace Pitcher.Models
 {
     public class Result
     {        
+        public int ID {get;set;}
         public int JobID {get;set;}
         
         public int ProblemID {get;set;}
         public Job Job {get;set;}
         public Problem Problem {get;set;}
-
     }
 }
