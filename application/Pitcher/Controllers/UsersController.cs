9941122adf8e3268102765d3346273b274959926
LayersOfAbstraction <jnash51@live.com>
2020-07-16 22:08:19 +1000

    Add Auth0 ManagementAPICLient backend

--- application/Pitcher/Controllers/UsersController.cs
+++ application/Pitcher/Controllers/UsersController.cs
@@ -17,8 +17,7 @@ namespace Pitcher.Controllers
 {
     
     public class UsersController : Controller
-    {
-        
+    {        
         private readonly TeamContext _context;
 
         public UsersController(TeamContext context)
@@ -74,17 +73,8 @@ namespace Pitcher.Controllers
         [Authorize(Roles = "admin")]
         public IActionResult Index()
         {
-            //PROBLEM: get all user profile values, email,name and role.
-            //EXTRACT claim from token.
-            //CREATE
-            foreach(var profile in User.Claims.ToList())
-            {
-                AuthenticationServiceUserProfile objAuth0Users = new AuthenticationServiceUserProfile();
-                {
-                    
-                }
-            }
-            string userSessionEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            //auth0 management API.
+            var client = new ManagementApiClient("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5UbEVRalpHUWpZM05VTTVOME5CTVVaQ05rTTRRa0kxTURnd01rVTNSREl4TTBRNFJqUTNOdyJ9.eyJpc3MiOiJodHRwczovL2Rldi1kZ2RmZ2ZkZ2YzMjQuYXUuYXV0aDAuY29tLyIsInN1YiI6IlRORmRQY2Y0WFJTM1NwR040WXlWTmxmOGdjaUJuQ0xkQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2Rldi1kZ2RmZ2ZkZ2YzMjQuYXUuYXV0aDAuY29tL2FwaS92Mi8iLCJpYXQiOjE1OTQ5MDA5MjQsImV4cCI6MTU5NDk4NzMyNCwiYXpwIjoiVE5GZFBjZjRYUlMzU3BHTjRZeVZObGY4Z2NpQm5DTGQiLCJzY29wZSI6InJlYWQ6Y2xpZW50X2dyYW50cyBjcmVhdGU6Y2xpZW50X2dyYW50cyBkZWxldGU6Y2xpZW50X2dyYW50cyB1cGRhdGU6Y2xpZW50X2dyYW50cyByZWFkOnVzZXJzIHVwZGF0ZTp1c2VycyBkZWxldGU6dXNlcnMgY3JlYXRlOnVzZXJzIHJlYWQ6dXNlcnNfYXBwX21ldGFkYXRhIHVwZGF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgZGVsZXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBjcmVhdGU6dXNlcnNfYXBwX21ldGFkYXRhIHJlYWQ6dXNlcl9jdXN0b21fYmxvY2tzIGNyZWF0ZTp1c2VyX2N1c3RvbV9ibG9ja3MgZGVsZXRlOnVzZXJfY3VzdG9tX2Jsb2NrcyBjcmVhdGU6dXNlcl90aWNrZXRzIHJlYWQ6Y2xpZW50cyB1cGRhdGU6Y2xpZW50cyBkZWxldGU6Y2xpZW50cyBjcmVhdGU6Y2xpZW50cyByZWFkOmNsaWVudF9rZXlzIHVwZGF0ZTpjbGllbnRfa2V5cyBkZWxldGU6Y2xpZW50X2tleXMgY3JlYXRlOmNsaWVudF9rZXlzIHJlYWQ6Y29ubmVjdGlvbnMgdXBkYXRlOmNvbm5lY3Rpb25zIGRlbGV0ZTpjb25uZWN0aW9ucyBjcmVhdGU6Y29ubmVjdGlvbnMgcmVhZDpyZXNvdXJjZV9zZXJ2ZXJzIHVwZGF0ZTpyZXNvdXJjZV9zZXJ2ZXJzIGRlbGV0ZTpyZXNvdXJjZV9zZXJ2ZXJzIGNyZWF0ZTpyZXNvdXJjZV9zZXJ2ZXJzIHJlYWQ6ZGV2aWNlX2NyZWRlbnRpYWxzIHVwZGF0ZTpkZXZpY2VfY3JlZGVudGlhbHMgZGVsZXRlOmRldmljZV9jcmVkZW50aWFscyBjcmVhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIHJlYWQ6cnVsZXMgdXBkYXRlOnJ1bGVzIGRlbGV0ZTpydWxlcyBjcmVhdGU6cnVsZXMgcmVhZDpydWxlc19jb25maWdzIHVwZGF0ZTpydWxlc19jb25maWdzIGRlbGV0ZTpydWxlc19jb25maWdzIHJlYWQ6aG9va3MgdXBkYXRlOmhvb2tzIGRlbGV0ZTpob29rcyBjcmVhdGU6aG9va3MgcmVhZDplbWFpbF9wcm92aWRlciB1cGRhdGU6ZW1haWxfcHJvdmlkZXIgZGVsZXRlOmVtYWlsX3Byb3ZpZGVyIGNyZWF0ZTplbWFpbF9wcm92aWRlciBibGFja2xpc3Q6dG9rZW5zIHJlYWQ6c3RhdHMgcmVhZDp0ZW5hbnRfc2V0dGluZ3MgdXBkYXRlOnRlbmFudF9zZXR0aW5ncyByZWFkOmxvZ3MgcmVhZDpzaGllbGRzIGNyZWF0ZTpzaGllbGRzIGRlbGV0ZTpzaGllbGRzIHJlYWQ6YW5vbWFseV9ibG9ja3MgZGVsZXRlOmFub21hbHlfYmxvY2tzIHVwZGF0ZTp0cmlnZ2VycyByZWFkOnRyaWdnZXJzIHJlYWQ6Z3JhbnRzIGRlbGV0ZTpncmFudHMgcmVhZDpndWFyZGlhbl9mYWN0b3JzIHVwZGF0ZTpndWFyZGlhbl9mYWN0b3JzIHJlYWQ6Z3VhcmRpYW5fZW5yb2xsbWVudHMgZGVsZXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRzIGNyZWF0ZTpndWFyZGlhbl9lbnJvbGxtZW50X3RpY2tldHMgcmVhZDp1c2VyX2lkcF90b2tlbnMgY3JlYXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgZGVsZXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgcmVhZDpjdXN0b21fZG9tYWlucyBkZWxldGU6Y3VzdG9tX2RvbWFpbnMgY3JlYXRlOmN1c3RvbV9kb21haW5zIHVwZGF0ZTpjdXN0b21fZG9tYWlucyByZWFkOmVtYWlsX3RlbXBsYXRlcyBjcmVhdGU6ZW1haWxfdGVtcGxhdGVzIHVwZGF0ZTplbWFpbF90ZW1wbGF0ZXMgcmVhZDptZmFfcG9saWNpZXMgdXBkYXRlOm1mYV9wb2xpY2llcyByZWFkOnJvbGVzIGNyZWF0ZTpyb2xlcyBkZWxldGU6cm9sZXMgdXBkYXRlOnJvbGVzIHJlYWQ6cHJvbXB0cyB1cGRhdGU6cHJvbXB0cyByZWFkOmJyYW5kaW5nIHVwZGF0ZTpicmFuZGluZyByZWFkOmxvZ19zdHJlYW1zIGNyZWF0ZTpsb2dfc3RyZWFtcyBkZWxldGU6bG9nX3N0cmVhbXMgdXBkYXRlOmxvZ19zdHJlYW1zIGNyZWF0ZTpzaWduaW5nX2tleXMgcmVhZDpzaWduaW5nX2tleXMgdXBkYXRlOnNpZ25pbmdfa2V5cyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.jkGTKtXN3fP6FFIZTYB8hbvqUoYJKx___YzMw5UXKQsdItXL_xtu8tvi1PM4INBttq81SFhWJvnfT_0YJ5B9lnD4FEPejvb6s_YU8AXTrP6EoKftR3o8DVtRsOIT-BkgI-vU_BNkSx8yJvBbca5wvfYFqylJEeHyTFQg9bajWc9WViHu9sEIph8xo5uUNIzgiRGQUZVbhNwPjJAdNgbsEy9P4Pez4LlpPJHbsxudPO3kgvehhaz3YnaO4PqovQwlpYtLkOOE6DvqJihCvKIKD8a5czLBctn0UFkaHPtVcenC9i92NOe3tH1Ygj9H6uY1hETU0fu0AkO_K3dHH0yz1w", "dev-dgdfgfdgf324.au.auth0.com");
             return View();
         }
 
