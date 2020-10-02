1a5cbbf9846894a683c1a40b73b61caad3b39b51
LayersOfAbstraction <jnash51@live.com>
2020-10-02 19:24:30 +1000

    Update models

--- application/Pitcher/Models/Job.cs
+++ application/Pitcher/Models/Job.cs
@@ -43,41 +43,5 @@ namespace Pitcher.Models
         public ICollection<Registration> Registrations {get;set;}
 
         public ICollection<Result> Results {get;set;}
-    }
-
-        public class Job2
-    {        
-        
-        public int ID { get; set; }
-
-        [Required]
-        [StringLength(20, MinimumLength = 3, ErrorMessage = "Job Title must be bettween 3 to 20 characters.")]
-        [DataType(DataType.Text)]
-        [Display(Name = "Job Title")]
-        [Column("JobTitle")]
-        public string JobTitle { get; set; }
-
-        [StringLength(200, MinimumLength = 3, ErrorMessage = "Job Description must be bettween 200 to 3 characters.")]
-        [DataType(DataType.Text)]
-        [Display(Name = "Description")]
-        [Column("JobDescription")]
-        public string JobDescription { get; set; }
-
-        [Required]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = " Start Date")]
-        [Column("JobStartDate")]
-        public DateTime JobStartDate {get;set;}
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Deadline Date")]
-        [Column("JobDeadlineDate")]
-        public DateTime JobDeadlineDate {get;set;}
-
-        [Display(Name = "Job Is Complete?")]
-        [Column("JobIsComplete")]
-        public bool JobIsComplete{get;set;}
-    }
+    }   
 }
--- application/Pitcher/Models/Registration.cs
+++ application/Pitcher/Models/Registration.cs
@@ -19,18 +19,4 @@ namespace Pitcher.Models
         public User User {get;set;}
         public Job Job {get;set;}
     }
-
-    public class Registration2
-    {
-        public int ID {get;set;}
-        public int UserID { get; set; }
-        public int JobID { get; set; }
-        
-        [Required]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "User Start Date")]
-        [Column("RegistrationDate")]
-        public DateTime RegistrationDate {get;set;}
-    }
 }
