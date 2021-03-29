8558d2af4a368923ad481058359ef350fac127c6
LayersOfAbstraction <jnash51@live.com>
2021-03-29 12:16:03 +1000

    add class, create table: tblAccessTokenCache and schema: security

---
+++ application/Pitcher/Data/AccessTokenCache.cs
@@ -0,0 +1,10 @@
+using System;
+
+internal partial class AccessTokenCache  
+{  
+    public string Id { get; set; }  
+    public byte[] Value { get; set; }  
+    public DateTimeOffset ExpiresAtTime { get; set; }  
+    public long? SlidingExpirationInSeconds { get; set; }  
+    public DateTimeOffset? AbsoluteExpiration { get; set; }  
+}  
