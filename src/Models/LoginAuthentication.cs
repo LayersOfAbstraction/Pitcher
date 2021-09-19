bb6e4d48837ee93af85198f17cac08e0e6495e7e
LayersOfAbstraction <jnash51@live.com>
2021-09-19 11:12:52 +1000

    make class more maintainable

--- src/Models/LoginAuthentication.cs
+++ src/Models/LoginAuthentication.cs
@@ -1,27 +1,31 @@
+using System.IO;
 using Auth0.AuthenticationApi;
-
+using Microsoft.Extensions.Configuration;
 
 namespace Pitcher.Models
 {
     public class LoginAuthentication
     {
+        //Allow value keys pair access to this class.
+        public static IConfiguration AppSetting { get; }
+
+        //Setup static constructor to get uri from appsettings
+        static LoginAuthentication()
+        {
+            AppSetting = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+        }
 
         public static Auth0Token Login(string ClientID, string ClientSecret, string domain)
         {
-            // var client = new RestClient($"https://{domain}/oauth/token");
-            // var request = new RestRequest(Method.POST);
-            // request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            // request.AddParameter("application/x-www-form-urlencoded", $"grant_type=client_credentials&client_id={ClientID}&client_secret={ClientSecret}&audience=https%3A%2F%2Fdev-dgdfgfdgf324.au.auth0.com%2Fapi%2Fv2%2F", ParameterType.RequestBody);
-            // IRestResponse response = client.Execute(request);
             var authenticationApiClient = new AuthenticationApiClient(domain);
-
-            // Get the access token
             var token =  authenticationApiClient.GetTokenAsync(new Auth0.AuthenticationApi.Models.ClientCredentialsTokenRequest
             {                
-                //Scope = 
                 ClientId = ClientID,
                 ClientSecret = ClientSecret,
-                Audience = "https://dev-dgdfgfdgf324.au.auth0.com/api/v2/"
+                Audience = LoginAuthentication.AppSetting["AccessTokenManagement:Audience"],
             }).Result;
             return new Auth0Token {strAuthToken = token.AccessToken};   
         }
