8f054df552462c4bf48af34825d34fa9c2eec337
LayersOfAbstraction <jnash51@live.com>
2021-10-24 21:09:36 +1000

    add file upload models

---
+++ src/Models/FileUploadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.MVC.Models
+{
+    public class FileUploadViewModel
+    {
+        public List<FileOnFileSystemModel> FilesOnFileSystem { get; set; }
+
+    }
+}
