a580de9989a64b0466c8aea496ed14a27e1bc656
Jordan Nash <jnash51@live.com>
2023-06-17 15:53:04 +1000

    Use ChatGPT to convert code to minimal hosting model.

--- src/Program.cs
+++ src/Program.cs
@@ -20,157 +20,117 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System.Data.Common;
 
-namespace Pitcher
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(options =>
 {
-    public class Program
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect("Auth0", options =>
     {
-        public static void Main(string[] args)
+        // Set the authority to your Auth0 domain
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+        // Configure the Auth0 Client ID and Client Secret
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+
+        // Set response type to code
+        options.ResponseType = "code";
+        // Configure the scope
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+        options.Scope.Add("openid email profile");
+        // Set the correct name claim type
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            var host = CreateHostBuilder(args).Build();
+            NameClaimType = "Name",
+            RoleClaimType = "https://schemas.Aussie_Tenant.com"
+        };
 
-            using (var scope = host.Services.CreateScope())
+        // Set the callback path, so Auth0 will call back to http://localhost:5001/callback
+        // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+        options.CallbackPath = new PathString("/callback");
+
+        // Configure the Claims Issuer to be Auth0
+        options.ClaimsIssuer = "Auth0";
+
+        // Saves tokens to the AuthenticationProperties
+        options.SaveTokens = true;
+
+        options.Events = new OpenIdConnectEvents
+        {
+            // handle the logout redirection
+            OnRedirectToIdentityProviderForSignOut = (context) =>
             {
-                var services = scope.ServiceProvider;
-                try
+                var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
+
+                var postLogoutUri = context.Properties.RedirectUri;
+                if (!string.IsNullOrEmpty(postLogoutUri))
                 {
-                    var context = services.GetRequiredService<TeamContext>();
-                    //DbInitializer.Initialize(context);
+                    if (postLogoutUri.StartsWith("/"))
+                    {
+                        // transform to absolute
+                        var request = context.Request;
+                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                    }
+                    logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
                 }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
+
+                context.Response.Redirect(logoutUri);
+                context.HandleResponse();
+
+                return Task.CompletedTask;
             }
+        };
+    });
 
-            host.Run();
-        }
+DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
+builder.Services.AddDbContext<TeamContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureServices((hostContext, services) =>
-                    {
-                        services.AddAuthentication(options =>
-                        {
-                            options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                            options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                            options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                        })
-                            .AddCookie()
-                            .AddOpenIdConnect("Auth0", options =>
-                            {
-                                // Set the authority to your Auth0 domain
-                                options.Authority = $"https://{hostContext.Configuration["Auth0:Domain"]}";
-                                // Configure the Auth0 Client ID and Client Secret
-                                options.ClientId = hostContext.Configuration["Auth0:ClientId"];
-                                options.ClientSecret = hostContext.Configuration["Auth0:ClientSecret"];
+// Add the whole configuration object here.
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
-                                // Set response type to code
-                                options.ResponseType = "code";
+// Add the Auth0 HttpClientManagementConnection.
+builder.Services.AddSingleton<IManagementConnection, HttpClientManagementConnection>();
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+        options.SerializerSettings.ContractResolver =
+            new CamelCasePropertyNamesContractResolver());
+builder.Services.AddAccessTokenManagement(builder.Configuration);
+builder.Services.AddTransient<IUserService, UserService>();
 
-                                // Configure the scope
-                                options.Scope.Clear();
-                                options.Scope.Add("openid");
-                                options.Scope.Add("profile");
-                                options.Scope.Add("email");
-                                options.Scope.Add("openid email profile");
-                                // Set the correct name claim type
-                                options.TokenValidationParameters = new TokenValidationParameters
-                                {
-                                    NameClaimType = "Name",
-                                    RoleClaimType = "https://schemas.Aussie_Tenant.com"
-                                };
+var app = builder.Build();
 
-                                // Set the callback path, so Auth0 will call back to http://localhost:5001/callback
-                                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
-                                options.CallbackPath = new PathString("/callback");
-
-                                // Configure the Claims Issuer to be Auth0
-                                options.ClaimsIssuer = "Auth0";
-
-                                // Saves tokens to the AuthenticationProperties
-                                options.SaveTokens = true;
-
-                                options.Events = new OpenIdConnectEvents
-                                {
-                                    // handle the logout redirection
-                                    OnRedirectToIdentityProviderForSignOut = (context) =>
-                                    {
-                                        var logoutUri =
-                                            $"https://{hostContext.Configuration["Auth0:Domain"]}/v2/logout?client_id={hostContext.Configuration["Auth0:ClientId"]}";
-
-                                        var postLogoutUri = context.Properties.RedirectUri;
-                                        if (!string.IsNullOrEmpty(postLogoutUri))
-                                        {
-                                            if (postLogoutUri.StartsWith("/"))
-                                            {
-                                                // transform to absolute
-                                                var request = context.Request;
-                                                postLogoutUri =
-                                                    request.Scheme + "://" + request.Host + request.PathBase +
-                                                    postLogoutUri;
-                                            }
-
-                                            logoutUri +=
-                                                $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-                                        }
-
-                                        context.Response.Redirect(logoutUri);
-                                        context.HandleResponse();
-
-                                        return Task.CompletedTask;
-                                    }
-                                };
-                            });
-
-                        DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
-                        services.AddDbContext<TeamContext>(options =>
-                            options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
-
-                        // Add the whole configuration object here.
-                        services.AddSingleton<IConfiguration>(hostContext.Configuration);
-
-                        // Add the Auth0 HttpClientManagementConnection.
-                        services.AddSingleton<IManagementConnection, HttpClientManagementConnection>();
-                        services.AddRazorPages();
-                        services.AddControllersWithViews()
-                            .AddNewtonsoftJson(options =>
-                                options.SerializerSettings.ContractResolver =
-                                    new CamelCasePropertyNamesContractResolver());
-                        services.AddAccessTokenManagement(hostContext.Configuration);
-                        services.AddTransient<IUserService, UserService>();
-                    })
-                    .Configure(app =>
-                    {
-                        var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
-                        if (env.IsDevelopment())
-                        {
-                            app.UseDeveloperExceptionPage();
-                        }
-                        else
-                        {
-                            app.UseExceptionHandler("/Home/Error");
-                            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                            app.UseHsts();
-                        }
-
-                        app.UseHttpsRedirection();
-                        app.UseStaticFiles();
-                        app.UseRouting();
-                        app.UseCookiePolicy();
-                        app.UseAuthentication();
-                        app.UseAuthorization();
-
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapControllers();
-                            endpoints.MapControllerRoute(
-                                name: "default",
-                                pattern: "{controller=Home}/{action=Index}/{id?}");
-                        });
-                    });
-                });
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
